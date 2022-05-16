9c5251dc162f3e1bb363a65677ab82aad8eca4b6
Mfoniso <ukpabiomfoniso@gmail.com>
2022-05-16 01:45:34 +0100

    styling components dynamically

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,20 +5,33 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [valid, setIsValid]= useState(true);
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
+    if (event.target.value.trim().length !== 0) {
+      setIsValid(true);
+    }
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+    
+
+    if (enteredValue.trim().length === 0) {
+      //in this case trim is used to remove the excess whitespace before the comparison
+      setIsValid(false)
+      return;
+    } else {
+      props.onAddGoal(enteredValue);
+    }
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
+      <div className={`form-control ${!valid ? ` invalid` : `` }` /* this is added in order to concatenate the css class invalid to the classname based on the defined conditon; and even more classes can still be added using the syntax and the particular condition order*/}>
+        
+        <label> Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import styled from "styled-components";
 
-import './Button.css';
+//this is a lot of synthetic sugar but this generates a class name specifically for this particular element
+//And this also aids in returning the elements called when creating the component e.g button, p etc.
+//the &-ampersand specifies that this style applies directly to the returned element which in the case is a button
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
